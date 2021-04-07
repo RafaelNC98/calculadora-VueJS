301c5b6463a7fb52533b0bc8e31dacd5eb9d90d1
RafaelNC98 <rafaelnadalinicarvalho@gmail.com>
2021-04-07 16:02:59 -0300

    finalizado..

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
-    <h1>Teste</h1>
+    <h1>Calculadora</h1>
+    <Calculator />
   </div>
 </template>
 
 <script>
-export default {
 
+import Calculator from "./main/Calculator"
+
+
+export default {
+  components: { Calculator }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/Roboto-Thin.ttf");
+}
 
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, #0f17c9, #cb009a, #ff0060, #ff6c29, #ffbc00);
+}
 </style>
